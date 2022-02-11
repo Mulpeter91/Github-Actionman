76a5358c3eba0f8bdfbf9d8803fe9f20353b9a0a
Rob Mulpeter <robertmulpetr@gmail.com>
2022-02-11 15:55:04 +0000

    add more variables

---
+++ Powershell/GithubEnvVariables.ps1
@@ -0,0 +1,14 @@
+"The owner and repository name.`n"
+"GITHUB_REPOSITORY: '$Env:GITHUB_REPOSITORY'`n`n"
+
+"The name of the event that triggered the workflow.`n"
+"GITHUB_EVENT_NAME: '$Env:GITHUB_EVENT_NAME'`n`n"
+
+"The job id you assigned to the current job.`n"
+"GITHUB_JOB: '$Env:GITHUB_JOB'`n`n"
+
+"A unique number for each workflow run within a repository. This number does not change if you re-run the workflow run.`n"
+"GITHUB_RUN_ID: '$Env:GITHUB_RUN_ID'`n`n"
+
+"A unique number for each run of a particular workflow in a repository. This number begins at 1 for the workflow's first run, and increments with each new run. This number does not change if you re-run the workflow run.`n"
+"GITHUB_RUN_NUMBER: '$Env:GITHUB_RUN_NUMBER'`n`n"
--- Scripts/GithubEnvVariables.ps1
+++
@@ -1,5 +0,0 @@
-"The name of the event that triggered the workflow.\n"
-"GITHUB_EVENT_NAME: $Env:GITHUB_EVENT_NAME\n\n"
-
-"The job id you assigned to the current job.\n"
-"GITHUB_JOB: $Env:GITHUB_JOB"
