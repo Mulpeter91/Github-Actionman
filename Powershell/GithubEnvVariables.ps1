e7fac5816a6bbd305940efc5071f56751e30ed79
Rob Mulpeter <robertmulpetr@gmail.com>
2022-02-12 09:40:21 +0000

    testing variables

--- Powershell/GithubEnvVariables.ps1
+++ Powershell/GithubEnvVariables.ps1
@@ -1,11 +1,5 @@
-"The owner and repository name."
+"`nThe owner and repository name."
 "GITHUB_REPOSITORY: '$Env:GITHUB_REPOSITORY'`n"
-
-"The name of the event that triggered the workflow."
-"GITHUB_EVENT_NAME: '$Env:GITHUB_EVENT_NAME'`n"
-
-"The branch or tag ref that triggered the workflow run. For branches this is the format refs/heads/<branch_name>, and for tags it is refs/tags/<tag_name>. This variable is only set if a branch or tag is available for the event type."
-"GITHUB_REF: '$Env:GITHUB_REF'`n"
 
 "The commit SHA that triggered the workflow."
 "GITHUB_SHA: '$Env:GITHUB_SHA'`n"
@@ -16,8 +10,12 @@
 "A unique number for each workflow run within a repository. This number does not change if you re-run the workflow run."
 "GITHUB_RUN_ID: '$Env:GITHUB_RUN_ID'`n"
 
-"A unique number for each run of a particular workflow in a repository. This number begins at 1 for the workflow's first run, and increments with each new run. This number does not change if you re-run the workflow run."
+"An unique number for each time the same workflow is re-run. Starts at 1 and increments by 1 on each re-run."
 "GITHUB_RUN_NUMBER: '$Env:GITHUB_RUN_NUMBER'`n"
 
 "The name of the runner executing the job."
 "RUNNER_NAME: '$Env:RUNNER_NAME'`n"
+
+$BEST_PINT
+$BEST_WHISKEY
+$BEST_COCKTAIL
