2db510bfc16b7c16a7b0ce3e1b374425993c0a5f
Rob Mulpeter <robertmulpetr@gmail.com>
2022-02-15 12:53:21 +0000

    test replace

--- Powershell/GithubApiRequest.ps1
+++ Powershell/GithubApiRequest.ps1
@@ -5,7 +5,7 @@ Write-Host $URI
 Write-Host $response
 
 "`n`nThis will return a specific open pull request.`n"
-$ID = $Env:GITHUB_REF_NAME - replace "/.*"
+$ID = $Env:GITHUB_REF_NAME -replace "/.*"
 $URI = "https://api.github.com/repos/$Env:GITHUB_REPOSITORY/pull/$ID"
 $response = Invoke-WebRequest -Uri $URI -Method Get -TimeoutSec 480
 Write-Host $URI
