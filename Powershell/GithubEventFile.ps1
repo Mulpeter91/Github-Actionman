4afc68a80dd3676cd429449d11a9ee6de25909d9
Rob Mulpeter <robertmulpetr@gmail.com>
2022-02-16 16:14:26 +0000

    test

--- Powershell/GithubEventFile.ps1
+++ Powershell/GithubEventFile.ps1
@@ -1,8 +1,5 @@
 "Event metadata file path: $Env:GITHUB_EVENT_PATH`n"
-$FILE_DATA = Get-Content -Path $Env:GITHUB_EVENT_PATH
+$EVENT_FILE = Get-Content -Path $Env:GITHUB_EVENT_PATH
 
-Write-Host $FILE_DATA
-$JSON = $FILE_DATA | ConvertFrom-Json
-Write-Host "TEST" $JSON
-"Sample output nodes:"
-"head_commit.author.username:" $JSON.head_commit.author.username
+$EVENT_JSON = $EVENT_FILE | ConvertFrom-Json
+Write-Host $EVENT_JSON.head_commit.author.username
