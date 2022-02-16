36e66405e969b0f6e5810a2c343d555f6ba618e8
Rob Mulpeter <robertmulpetr@gmail.com>
2022-02-16 16:09:26 +0000

    test

--- Powershell/GithubEventFile.ps1
+++ Powershell/GithubEventFile.ps1
@@ -3,5 +3,6 @@ $FILE_DATA = Get-Content -Path $Env:GITHUB_EVENT_PATH
 
 Write-Host $FILE_DATA
 $JSON = $FILE_DATA | ConvertFrom-Json
+Write-Host "TEST" $JSON
 "Sample output nodes:"
 "head_commit.author.username:" $JSON.head_commit.author.username
