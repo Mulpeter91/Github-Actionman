7ac4dcfbfa66e88100b6955bf171965bdf79df09
Rob Mulpeter <robertmulpetr@gmail.com>
2022-02-17 11:43:10 +0000

    test

--- Powershell/GithubWebRequests.ps1
+++ Powershell/GithubWebRequests.ps1
@@ -2,21 +2,21 @@
 $URI = "https://api.github.com/repos/$Env:GITHUB_REPOSITORY/pulls`n"
 Write-Host $URI
 
-"`n`nThis will return all pull requests of a specified state:"
+"`nThis will return all pull requests of a specified state:"
 $URI = "https://api.github.com/repos/$Env:GITHUB_REPOSITORY/pulls?state=$Env:PR_STATE`n"
 Write-Host $URI
 
-"`n`nThis will return a specific pull request:"
+"`nThis will return a specific pull request:"
 $PR_NUMBER = $Env:GITHUB_REF_NAME -replace "/.*" # <- You can also get the PR number from the pull request event file.
 $URI = "https://api.github.com/repos/$Env:GITHUB_REPOSITORY/pulls/$PR_NUMBER`n"
 Write-Host $URI
 $RESPONSE = Invoke-WebRequest -Uri $URI -Method Get -TimeoutSec 480
 Write-Host $RESPONSE
 
-"`n`nAccessing variables from the object: "
+"`nAccessing variables from the object: "
 $JSON_OBJECT = $RESPONSE | ConvertFrom-Json
-"HTML URL: ${ $JSON_OBJECT.html_url }"
-"TITLE: ${ $JSON_OBJECT.title}"
+Write-Host "HTML URL: ${ $JSON_OBJECT.html_url }"
+Write-Host "TITLE: ${ $JSON_OBJECT.title}"
 Write-Host "BODY:" $JSON_OBJECT.body
 Write-Host "USER:" $JSON_OBJECT.user.login
 Write-Host "REQUESTED REVIEWERS:" $JSON_OBJECT.requested_reviewers
