931fe44eb934fd8499048d38525f4b9cc01f7d59
Rob Mulpeter <robertmulpetr@gmail.com>
2022-02-17 11:36:24 +0000

    test

--- Powershell/GithubWebRequests.ps1
+++ Powershell/GithubWebRequests.ps1
@@ -1,15 +1,13 @@
 "`nThis will return a list of all open pull requests:"
 $URI = "https://api.github.com/repos/$Env:GITHUB_REPOSITORY/pulls`n"
 Write-Host $URI
-$RESPONSE = Invoke-WebRequest -Uri $URI -Method Get -TimeoutSec 480
-Write-Host $RESPONSE
 
 "`n`nThis will return all pull requests of a specified state:"
 $URI = "https://api.github.com/repos/$Env:GITHUB_REPOSITORY/pulls?state=$Env:PR_STATE`n"
 Write-Host $URI
 
 "`n`nThis will return a specific pull request:"
-$PR_NUMBER = $Env:GITHUB_REF_NAME -replace "/.*" # <- You can also get the PR number from the event file.
+$PR_NUMBER = $Env:GITHUB_REF_NAME -replace "/.*" # <- You can also get the PR number from the pull request event file.
 $URI = "https://api.github.com/repos/$Env:GITHUB_REPOSITORY/pulls/$PR_NUMBER`n"
 Write-Host $URI
 $RESPONSE = Invoke-WebRequest -Uri $URI -Method Get -TimeoutSec 480
